7176efea4706b095c9152a26e882058db2d0d0db
Ivan <idoric1@gmail.com>
2015-03-25 10:04:03 +0100

    Changed Guplfile.js so that livereload would reload just changed files and not all of them

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 
-gulp.task('liver', function() { 
-    return gulp.src(['style.css', '*.php', './js/*.js']).pipe(livereload());
-});
-
 gulp.task('watch', function(){
     livereload.listen();
-    gulp.watch(['style.css', '*.php', './js/*.js'], ['liver']);
+    gulp.watch(['style.css', '*.php', './js/*.js', './parts/*.php'], function (files){
+        livereload.changed(files)
+    });
 });
